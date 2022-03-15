b43d96f63a8fe9c175f3315d51c1cf8a84efe0e2
dcz <gihu.dcz@porcupinefactory.org>
2022-03-15 15:08:47 +0000

    block userspace driver

--- capsules/src/block_storage_driver.rs
+++ capsules/src/block_storage_driver.rs
@@ -15,9 +15,10 @@ use core::cmp;
 
 use kernel::grant::{AllowRoCount, AllowRwCount, Grant, UpcallCount};
 use kernel::hil;
+use kernel::hil::block_storage::{BlockIndex, Region};
 use kernel::processbuffer::{ReadableProcessBuffer, WriteableProcessBuffer};
 use kernel::syscall::{CommandReturn, SyscallDriver};
-use kernel::utilities::cells::{OptionalCell, TakeCell};
+use kernel::utilities::cells::TakeCell;
 use kernel::{ErrorCode, ProcessId};
 
 /// Syscall driver number.
@@ -25,10 +26,36 @@ use crate::driver;
 pub const DRIVER_NUM: usize = driver::NUM::NvmStorage as usize;
 
 enum Command {
-    READ_RANGE = 0,
-    READ = 1,
-    ERASE = 2,
-    WRITE = 3,
+    CHECK = 0,
+    /// device size in bytes
+    SIZE = 1,
+    /// Size of write, erase blocks.
+    /// Separate from size because doesn't fit in one return call.
+    GEOMETRY = 2,
+    /// Read an arbitrary range from an arbitrary address.
+    READ_RANGE = 3,
+    /// Read a single write block at given block index.
+    READ = 4,
+    /// Erase a single erase block at given block index.
+    ERASE = 5,
+    /// Write a single write block at given block index.
+    WRITE = 6,
+}
+
+impl TryFrom<usize> for Command {
+    type Error = ();
+    fn try_from(v: usize) -> Result<Self, Self::Error> {
+        match v {
+            0 => Ok(Command::CHECK),
+            1 => Ok(Command::SIZE),
+            2 => Ok(Command::GEOMETRY),
+            3 => Ok(Command::READ_RANGE),
+            4 => Ok(Command::READ),
+            5 => Ok(Command::ERASE),
+            6 => Ok(Command::WRITE),
+            _ => Err(()),
+        }
+    }
 }
 
 /// Ids for read-only allow buffers
@@ -51,82 +78,125 @@ enum Upcall {
     WRITE = 2,
 }
 
+const UPCALL_COUNT: usize = 3;
+
 pub static mut BUFFER: [u8; 512] = [0; 512];
 
-#[derive(Clone, Copy, PartialEq)]
-pub enum NonvolatileCommand {
-    UserspaceRead,
-    UserspaceWrite,
-    KernelRead,
-    KernelWrite,
+#[derive(Clone, Copy)]
+enum Read {
+    None,
+    Requested(ProcessId),
 }
 
 #[derive(Clone, Copy)]
-pub enum NonvolatileUser {
-    App { app_id: ProcessId },
-    Kernel,
+struct State {
+    read: Read,
 }
 
-pub struct App {
-    pending_command: bool,
-    command: NonvolatileCommand,
-    offset: usize,
-    length: usize,
+/// Userspace interface for `hil::block_storage::BlockStorage`.
+///
+/// Supports only one command of a given type at a time
+/// (but may support only one command in flight at all,
+/// if that's what the underlying device does).
+///
+/// Requires a buffer of size at least `W`.
+///
+/// `W` is the size of a write block, `E` is the erase block size.
+pub struct BlockStorage<'a, T, const W: u32, const E: u32>
+where
+    T: hil::block_storage::BlockStorage<W, E>,
+{
+    // The underlying physical storage device.
+    device: &'a T,
+    // Per-app state.
+    apps: Grant<
+        (),
+        UpcallCount<UPCALL_COUNT>,
+        AllowRoCount<{ ro_allow::COUNT }>,
+        AllowRwCount<{ rw_allow::COUNT }>,
+    >,
+    state: Cell<State>,
+    buffer: TakeCell<'static, [u8]>,
 }
 
-impl Default for App {
-    fn default() -> App {
-        App {
-            pending_command: false,
-            command: NonvolatileCommand::UserspaceRead,
-            offset: 0,
-            length: 0,
+impl<T, const W: u32, const E: u32> BlockStorage<'_, T, W, E>
+where
+    T: hil::block_storage::BlockStorage<W, E>,
+{
+    fn start_read(&self, region: &Region<W>, appid: ProcessId)
+        -> Result<(), ErrorCode>
+    {
+        let state = self.state.get();
+        match state.read {
+            Read::Requested(..) => Err(ErrorCode::BUSY),
+            Read::None => self.buffer.take().map_or_else(
+                || Err(ErrorCode::NOMEM),
+                |buffer| {
+                    match self.device.read(region, buffer) {
+                        Ok(()) => {
+                            self.state.set(State {
+                                read: Read::Requested(appid),
+                                ..state
+                            });
+                            Ok(())
+                        },
+                        Err((e, buf)) => {
+                            self.buffer.replace(buf);
+                            Err(e)
+                        }
+                    }
+                }
+            ),
         }
     }
 }
 
-/// Userspace interface for `hil::block_storage::BlockStorage`
-pub struct BlockStorage<T, const W: u32, const E: u32>
-    where T: hil::block_storage::BlockStorage<W, E>
-{
-    // The underlying physical storage device.
-    device: &'a dyn hil::nonvolatile_storage::NonvolatileStorage<'static>,
-    // Per-app state.
-    apps: Grant<
-        App,
-        UpcallCount<3>,
-        AllowRoCount<{ ro_allow::COUNT }>,
-        AllowRwCount<{ rw_allow::COUNT }>,
-    >,
-}
-
-impl<T: hil::block_storage::BlockStorage> hil::block_storage::Client
-    for BlockStorage<T>
+impl<T, const W: u32, const E: u32> hil::block_storage::Client<W, E> for BlockStorage<'_, T, W, E>
+where
+    T: hil::block_storage::BlockStorage<W, E>,
 {
     fn read_complete(&self, read_buffer: &'static mut [u8], ret: Result<(), ErrorCode>) {
         let state = self.state.get();
         match state.read {
-            Read::Requested(length) => self.current_user.take().map(|app_id| {
-                self.apps.enter(app_id, move |_, kernel_data| {
-                    // Need to copy in the contents of the buffer
-                    kernel_data
-                        .get_readwrite_processbuffer(rw_allow::READ)
-                        .and_then(|read| {
-                            read.mut_enter(|app_buffer| {
-                                let read_len = cmp::min(app_buffer.len(), length);
+            Read::Requested(app_id) => {
+                self
+                    .apps
+                    .enter(app_id, move |_, kernel_data| {
+                        let ret = match ret {
+                            Ok(()) => {
+                                // Need to copy in the contents of the buffer
+                                kernel_data
+                                    .get_readwrite_processbuffer(rw_allow::READ)
+                                    .and_then(|read| {
+                                        read.mut_enter(|app_buffer| {
+                                            let read_len = cmp::min(app_buffer.len(), W as usize);
 
-                                let d = &mut app_buffer[0..(read_len as usize)];
-                                d.copy_from_slice(&buffer[0..read_len]);
-                            })
+                                            let d = &app_buffer[0..(read_len)];
+                                            d.copy_from_slice(&read_buffer[0..read_len]);
+                                        })
+                                    })
+                                    .map_err(ErrorCode::from)
+                            },
+                            Err(e) => Err(e),
+                        };
+
+                        // Replace the buffer we used to do this read.
+                        self.buffer.replace(read_buffer);
+                        self.state.set(State {
+                            read: Read::None,
+                            ..state
                         });
-
-                    // Replace the buffer we used to do this read.
-                    self.buffer.replace(buffer);
-
-                    // And then signal the app.
-                    kernel_data.schedule_upcall(0, (length, 0, 0)).ok();
-                });
-            }),
+                        
+                        // And then signal the app.
+                        let upcall_data = ret.map_or_else(
+                            |e| (1, e.into(), 0),
+                            |()| (0, 0, 0),
+                        );
+                        kernel_data.schedule_upcall(Upcall::READ as usize, upcall_data)
+                            .unwrap_or_else(|e| kernel::debug!("Can't upcall: {:?}", e))
+                    })
+                    .unwrap_or_else(|e| kernel::debug!("Can't get grant: {:?}", e))
+            },
             _ => kernel::debug!("Unexpected read reply"),
         }
     }
@@ -142,84 +212,45 @@ impl<T: hil::block_storage::BlockStorage> hil::block_storage::Client
     fn erase_complete(&self, ret: Result<(), ErrorCode>) {}
 }
 
-impl<T: hil::block_storage::BlockStorage> SyscallDriver for BlockStorage<T> {
-    /// Setup shared kernel-writable buffers.
-    ///
-    /// ### `allow_num`
-    ///
-    /// - `0`: Setup a buffer to read from the nonvolatile storage into.
-
-    /// Setup shared kernel-readable buffers.
-    ///
-    /// ### `allow_num`
-    ///
-    /// - `0`: Setup a buffer to write bytes to the nonvolatile storage.
-
-    // Setup callbacks.
-    //
-    // ### `subscribe_num`
-    //
-    // - `0`: Setup a read done callback.
-    // - `1`: Setup a write done callback.
-
-    /// Command interface.
-    ///
-    /// Commands are selected by the lowest 8 bits of the first argument.
-    ///
-    /// ### `command_num`
-    ///
-    /// - `0`: Return Ok(()) if this driver is included on the platform.
-    /// - `1`: Return the number of bytes available to userspace.
-    /// - `2`: Start a read from the nonvolatile storage.
-    /// - `3`: Start a write to the nonvolatile_storage.
+impl<T, const W: u32, const E: u32> SyscallDriver for BlockStorage<'_, T, W, E>
+where
+    T: hil::block_storage::BlockStorage<W, E>,
+{
     fn command(
         &self,
         command_num: usize,
         offset: usize,
-        length: usize,
+        _length: usize,
         appid: ProcessId,
     ) -> CommandReturn {
-        match command_num {
-            0 /* This driver exists. */ => {
+        match Command::try_from(command_num) {
+            Ok(Command::CHECK) => {
                 CommandReturn::success()
-            }
-
-            1 /* How many bytes are accessible from userspace */ => {
-                // TODO: Would break on 64-bit platforms
-                CommandReturn::success_u32(self.userspace_length as u32)
             },
-
-            2 /* Issue a read command */ => {
-                let res =
-                    self.enqueue_command(
-                        NonvolatileCommand::UserspaceRead,
-                        offset,
-                        length,
-                        Some(appid),
-                    );
-
-                match res {
-                    Ok(()) => CommandReturn::success(),
-                    Err(e) => CommandReturn::failure(e),
-                }
+            Ok(Command::SIZE) => {
+                self.device.get_size()
+                    .map_or_else(CommandReturn::failure, CommandReturn::success_u64)
+            },
+            Ok(Command::GEOMETRY) => {
+                CommandReturn::success_u32_u32(W, E)
+            },
+            Ok(Command::READ_RANGE) => {
+                CommandReturn::failure(ErrorCode::NOSUPPORT)
+            },
+            Ok(Command::READ) => {
+                let region = Region {
+                    index: BlockIndex(offset as u32),
+                    length_blocks: 1,
+                };
+                self.start_read(&region, appid).into()
+            },
+            Ok(Command::ERASE) => {
+                CommandReturn::failure(ErrorCode::NOSUPPORT)
+            },
+            Ok(Command::WRITE) => {
+                CommandReturn::failure(ErrorCode::NOSUPPORT)
             }
-
-            3 /* Issue a write command */ => {
-                let res =
-                    self.enqueue_command(
-                        NonvolatileCommand::UserspaceWrite,
-                        offset,
-                        length,
-                        Some(appid),
-                    );
-
-                match res {
-                    Ok(()) => CommandReturn::success(),
-                    Err(e) => CommandReturn::failure(e),
-                }
-            }
-
-            _ => CommandReturn::failure(ErrorCode::NOSUPPORT),
+            Err(()) => CommandReturn::failure(ErrorCode::NOSUPPORT),
         }
     }
 
